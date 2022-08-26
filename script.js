039067a8dfc1d78db8ec84411251d6babf5352d9
JusmanEfendy <njhussrhiee0206@gmail.com>
2022-08-25 21:58:00 -0500

    ngacooxx

---
+++ script.js
@@ -0,0 +1,7 @@
+const redirInstagram = () => {
+  window.location.href = "https://www.instagram.com/jussy_ez/"
+}
+
+const redirWhatsapp = () => {
+  window.location.href = "https://wa.me/+6283192847276"
+}
